30d29e8fc2797784dd43e97622c540b6dcebb6a7
Robert Engelhard <robenge@dfds.com>
2022-09-08 10:56:38 +0200

    initial

---
+++ ComicsOrganizer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComicsOrganizer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
