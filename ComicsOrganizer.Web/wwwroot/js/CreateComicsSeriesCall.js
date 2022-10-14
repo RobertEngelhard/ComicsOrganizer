a92384e2210c6c519ce91fb20e83bc1ed06b70f2
toske <toske@dfds.com>
2022-10-14 15:10:54 +0200

    #1 Fixed the ajax call

---
+++ ComicsOrganizer.Web/wwwroot/js/CreateComicsSeriesCall.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $('#submit').on('click', function (evt) {
+        document.querySelector('#submit').disabled = true;
+        console.log("is it here?");
+
+        var request = $.ajax(
+            {
+                method: "POST",
+                url: "https://localhost:7122/ComicsSeries",
+                data: JSON.stringify({ name: "John" }),
+                contentType: "application/json",
+                dataType: "json"
+            });
+
+        request.done(function (msg) {
+            alert("Data Saved: " + msg);
+        })
+
+        request.fail(function (jqXHR, textStatus) {
+            alert("Request failed: " + textStatus + " Error Message: " + jqXHR.responseText);
+        })
+    });
+});
